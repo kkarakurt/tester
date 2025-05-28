93cc58cd6ddf9378d8b346cb800f1769271f925f
Kağan Karakurt <124518547+kkarakurt@users.noreply.github.com>
2025-05-29 00:16:00 +0300

    bitik cd.py

--- cd.py
+++ cd.py
@@ -1,6 +1,3 @@
-from ab import *
-import time
-
 def GeneticAlgorithm(cities, generations, sample_size, mutation_prob):
     city_names = list(cities.keys())
     sample = [random.sample(list(city_names), len(city_names)) for i in range(sample_size)]
@@ -30,36 +27,3 @@ def GeneticAlgorithm(cities, generations, sample_size, mutation_prob):
     best_distance = distance_calculator(best_queue, cities)
     print("Best Solution (genetic):", best_queue)
     print("Shortest Distance (genetic):", round(best_distance, 2))
-
-# --- Ana program (main) ---
-
-cities = {
-    'A': (16.47, 96.10),
-    'B': (14.05, 98.12),
-    'C': (20.09, 92.54),
-    'D': (22.39, 93.37),
-    'E': (25.23, 97.24),
-    'F': (22.00, 96.05),
-    'G': (20.47, 97.02),
-    'H': (17.20, 96.29),
-    'I': (16.30, 97.38)
-}
-
-sample = 5
-generations = 100
-mutation_prob = 0.05  # yüzde 5
-
-k = 100
-
-start_time_swap = time.time()
-SingleSwapSearch(cities, k)
-end_time_swap = time.time()
-duration_swap = (end_time_swap - start_time_swap)*1000
-print(f"Single Swap Search süresi: ", round(duration_swap, 2), "Ms\n")
-
-# --- Genetic Algorithm Süresi ---
-start_time_genetic = time.time()
-GeneticAlgorithm(cities, generations, sample, mutation_prob)
-end_time_genetic = time.time()
-duration_genetic = (end_time_genetic-start_time_genetic)*1000
-print("Genetik Algoritma süresi: ", round(duration_genetic, 2), "Ms")
